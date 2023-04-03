14fdfc107f9706efd51440407dcfc6112df015a2
EC2 Default User <125953174+sksk124@users.noreply.github.com>
2023-04-03 13:56:09 +0000

    Update cart item

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -26,9 +26,6 @@ class Public::CartItemsController < ApplicationController
   redirect_to cart_items_path, notice: "カート内の商品を更新しました。"
  end
  
- 
- 
- 
 
   def destroy
    @cart_item = current_customer.cart_items.find(params[:id])
